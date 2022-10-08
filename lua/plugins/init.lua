ff4007367b00f41b5bc682660065ced2eeb97999
Brian Tipton <briantiptondev@gmail.com>
2022-10-08 14:47:43 -0500

    pathing was hard coded in extensions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,7 +24,7 @@ local plugins = {
   },
 
   ["SigmaNvim/UI"] = {
-    after = "base46",
+    after = "Base46",
     config = function()
       local present, sigma_ui = pcall(require, "sigma_ui")
 
@@ -151,7 +151,7 @@ local plugins = {
   },
 
   ["goolord/alpha-nvim"] = {
-    after = "base46",
+    after = "Base46",
     disable = true,
     config = function()
       require "plugins.configs.alpha"
